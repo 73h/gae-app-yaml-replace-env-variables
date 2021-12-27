23998ffc26dacfd3c81c5a1b34516dfe0d8cbdce
h73 <heikoschmidt82@gmail.com>
2021-12-27 18:17:16 +0100

    workflow test

--- app/main.py
+++ app/main.py
@@ -1,12 +1,24 @@
 import os
 
-if __name__ == "__main__":
+
+def replace_env_variables_in_app_yaml_file():
     github_workspace = os.environ.get("GITHUB_WORKSPACE")
     app_yaml_path = os.environ.get("INPUT_APP_YAML_PATH")
 
     yaml_file_path = os.path.join(github_workspace, app_yaml_path)
-    print(yaml_file_path)
 
-    yaml_file = open(yaml_file_path, "r")
-    print(yaml_file.read())
-   
+    print(f"open {yaml_file_path}")
+    yaml_file = open(yaml_file_path, "r+")
+    yaml = yaml_file.read()
+
+    print(os.environ.get("VAR_ONE"))
+
+    # yaml_file.write(yaml)
+    yaml_file.close()
+
+    test = open(yaml_file_path, "r")
+    # print(test.read())
+
+
+if __name__ == "__main__":
+    replace_env_variables_in_app_yaml_file()
