aac4bf471dddea99937064cf0bc46f37c1280ad3
h73 <heikoschmidt82@gmail.com>
2021-12-27 15:45:42 +0100

    ...

--- app/main.py
+++ app/main.py
@@ -1,2 +1,7 @@
+import os
+
 if __name__ == "__main__":
-    print(73)
+    yaml_file_path = os.environ.get("app_yaml_path")
+    print(yaml_file_path)
+    yaml_file = open(os.environ.get("app_yaml_path"), "r")
+    print(yaml_file.read())
