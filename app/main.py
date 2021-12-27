673630273cdce6aa07f814cd22f6296b9e404a04
h73 <heikoschmidt82@gmail.com>
2021-12-27 21:04:16 +0100

    refactor main.py

--- app/main.py
+++ app/main.py
@@ -12,20 +12,17 @@ def replace_env_variables_in_app_yaml_file():
     with open(yaml_file, "r") as stream:
         yaml_data = yaml.safe_load(stream)
 
-    if "env_variables" in yaml_data:
-        env_variables = yaml_data["env_variables"]
-    else:
+    if "env_variables" not in yaml_data:
         raise Exception("cannot find the \"env_variables\" section in yaml-file")
 
-    for key in env_variables:
-        env_var = env_variables[key]
+    for key in yaml_data["env_variables"]:
+        env_var = yaml_data["env_variables"][key]
         if env_var.startswith("$"):
             repl_env_var = os.environ.get(env_var[1:])
             if repl_env_var is not None:
                 yaml_data["env_variables"][key] = repl_env_var
             else:
                 raise Exception(f"cannot find the env-variable {key} in \"env\" section in github workflow")
-        pass
 
     with open(yaml_file, "w") as stream:
         yaml.dump(yaml_data, stream)
