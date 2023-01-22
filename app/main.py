05b7b6a93d8cbff0a024c9b006264f953b2b78cb
Zdenek Hasek <me@zdehasek.com>
2023-01-22 13:28:15 +0100

    reverting

--- app/main.py
+++ app/main.py
@@ -25,7 +25,7 @@ def replace_env_variables_in_app_yaml_file():
                 raise Exception(f"cannot find the env-variable {env_var[1:]} in \"env\" section in github workflow")
 
     with open(yaml_file, "w") as stream:
-        yaml.dump(yaml_data, stream, width=float("inf"), default_style='"')
+        yaml.dump(yaml_data, stream, width=float("inf"))
 
 
 if __name__ == "__main__":
