28af8302b636f48c4b022663c8bc175a2d0c1fbc
Zdenek Hasek <me@zdehasek.com>
2023-01-23 14:13:15 +0100

    trying to fix double quotes problem

--- app/main.py
+++ app/main.py
@@ -1,7 +1,11 @@
+
 import os
 
-import yaml
-import json
+import ruamel.yaml
+
+yaml = ruamel.yaml.YAML()
+yaml.preserve_quotes = True
+
 
 def replace_env_variables_in_app_yaml_file():
     github_workspace = os.environ.get("GITHUB_WORKSPACE")
@@ -25,7 +29,7 @@ def replace_env_variables_in_app_yaml_file():
                 raise Exception(f"cannot find the env-variable {env_var[1:]} in \"env\" section in github workflow")
 
     with open(yaml_file, "w") as stream:
-        yaml.dump(json.dumps(yaml_data, ensure_ascii=False), stream, width=float("inf"))
+        yaml.dump(yaml_data, stream, width=float("inf"))
 
 
 if __name__ == "__main__":
